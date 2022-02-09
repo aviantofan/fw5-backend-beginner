fc6b801bfb6c355cbd33ddc957b8e48b641f4eb7
eexxprd <aviantofan@gmail.com>
2022-02-09 20:59:20 +0700

    fix: fix error in routes histories

--- source/routes/histories.js
+++ source/routes/histories.js
@@ -1,9 +1,8 @@
 const histories = require('express').Router();
 
-const {getHistories, getPopulars, getHistory, postHistory, patchHistory, deleteHistory} = require('../controllers/histories');
+const {getHistories, getHistory, postHistory, patchHistory, deleteHistory} = require('../controllers/histories');
 
 histories.get('/', getHistories);
-histories.get('/populars', getPopulars);
 histories.get('/:id', getHistory);
 histories.post('/', postHistory);
 histories.patch('/:id', patchHistory);
