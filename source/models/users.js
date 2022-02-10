7a7486cc5b456226948b0d24296585180c201966
eexxprd <aviantofan@gmail.com>
2022-02-10 22:31:18 +0700

    fix: fix models add models for login and authentication

--- source/models/users.js
+++ source/models/users.js
@@ -29,13 +29,29 @@ exports.getUserCheck = (data,cb) => {
     return(db);
 };
 
-exports.postUser = (data, cb) =>{
-    db.query('INSERT INTO users SET ?', data, (err, res) => {
-        if(err) throw err;
-        cb(res);
+// exports.postUser = (data, cb) =>{
+//     db.query('INSERT INTO users SET ?', data, (err, res) => {
+//         if(err) throw err;
+//         cb(res);
+//     });
+//     return (db);
+// };
+
+exports.postUser = (data) => new Promise ((resolve, reject) => {
+    db.query(`INSERT INTO users (name, email, username, password, gender, address, phone, birthdate) VALUES 
+  ('${data.name}', '${data.email}', '${data.username}', '${data.password}', '${data.gender}', '${data.address}', '${data.phone}', '${data.birthdate}')`,
+    (err, res) => {
+        if (err) reject (err);
+        resolve(res);
     });
-    return (db);
-};
+});
+
+exports.getPostUser = () => new Promise ((resolve, reject) => {
+    db.query('SELECT * FROM users ORDER BY id DESC LIMIT 1', (err, res) => {
+        if (err) reject (err);
+        resolve(res);
+    });
+});
 
 exports.patchUser = (data, id, cb) => {
     db.query('UPDATE users SET ? WHERE id=?', [data, id], (err, res) => {
