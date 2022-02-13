5c45800cedcc5c0f31b8d5a05bdf866a295e44ac
eexxprd <aviantofan@gmail.com>
2022-02-13 10:55:09 +0700

    feat: add verifyUser

--- source/routes/categories.js
+++ source/routes/categories.js
@@ -1,11 +1,12 @@
 const categories = require('express').Router();
 
-const {getCategories, getCategory, postCategory, patchCategory, deleteCategory} = require('../controllers/categories');
+const { getCategories, getCategory, postCategory, patchCategory, deleteCategory } = require('../controllers/categories');
+const { verifyUser } = require('../helpers/auth');
 
-categories.get('/', getCategories);
-categories.post('/', postCategory);
-categories.get('/:id', getCategory);
-categories.patch('/:id', patchCategory);
-categories.delete('/:id', deleteCategory);
+categories.get('/', verifyUser, getCategories);
+categories.post('/', verifyUser, postCategory);
+categories.get('/:id', verifyUser, getCategory);
+categories.patch('/:id', verifyUser, patchCategory);
+categories.delete('/:id', verifyUser, deleteCategory);
 
 module.exports = categories;
