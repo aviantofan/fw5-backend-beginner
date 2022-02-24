af787c2f02f1d155ed32fcffc40cb50852d7fed0
eexxprd <aviantofan@gmail.com>
2022-02-24 11:12:46 +0700

    fix: fix problems

--- source/routes/vehicles.js
+++ source/routes/vehicles.js
@@ -3,10 +3,10 @@ const vehicles = require('express').Router();
 const { getVehicles, getPopulars, getVehiclesCategory, getVehicle, postVehicle, patchVehicle, deleteVehicle } = require('../controllers/vehicles');
 const { verifyUser } = require('../helpers/auth');
 
-vehicles.get('/', verifyUser, getVehicles);
+vehicles.get('/', getVehicles);
 vehicles.post('/', verifyUser, postVehicle);
 vehicles.get('/category', verifyUser, getVehiclesCategory);
-vehicles.get('/:id', verifyUser, getVehicle);
+vehicles.get('/:id', getVehicle);
 vehicles.patch('/:id', verifyUser, patchVehicle);
 vehicles.delete('/:id', verifyUser, deleteVehicle);
 vehicles.get('/populars', verifyUser, getPopulars);
