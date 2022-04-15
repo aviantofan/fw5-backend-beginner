663a405307107f2272d113eb614e31d3b4581e51
eexxprd <aviantofan@gmail.com>
2022-04-15 14:25:41 +0700

    feat: add filter size

--- source/helpers/upload.js
+++ source/helpers/upload.js
@@ -8,23 +8,29 @@ const storage = multer.diskStorage({
     const _file = file.originalname.split('.');
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, _file[0] + '-' + uniqueSuffix + '.' + _file[1]);
-  } 
+  }
 });
 
 
 const fileFilter = (req, file, cb) => {
   const supportedMime = [
-    'image/jpeg', 
-    'image/png', 
+    'image/jpeg',
+    'image/png',
     'image/gif'
   ];
-  if(!supportedMime.includes(file.mimetype)){
+  if (!supportedMime.includes(file.mimetype)) {
     cb(new Error('Filetype mismatch!'), false);
-  } else{
+  } else {
     cb(null, true);
   }
 };
 
-const upload = multer({storage: storage, fileFilter });
+const upload = multer({
+  storage: storage,
+  fileFilter,
+  limits: {
+    fileSize: 2097152 // 2MB
+  }
+});
 
 module.exports = upload;
