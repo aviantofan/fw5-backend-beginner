d421ac3a215f9085b74b505637f0befb6f8f45a5
eexxprd <aviantofan@gmail.com>
2022-04-17 01:50:36 +0700

    feat: add validation and standard response

--- source/controllers/histories.js
+++ source/controllers/histories.js
@@ -1,4 +1,69 @@
 const historyModel = require('../models/histories');
+const response = require('../helpers/response');
+const validator = require('validator');
+const { APP_URL } = process.env;
+
+exports.postHistory = (req, res) => {
+  const data = {
+    userId: req.body.userId,
+    vehicleId: req.body.vehicleId,
+    rentStartDate: req.body.rentStartDate,
+    rentEndDate: req.body.rentEndDate,
+    prepayment: req.body.prepayment,
+    isReturned: req.body.isReturned
+  };
+  if (validator.isEmpty(data.userId)) {
+    return response(res, 'User Id cannot empty!', null, null, 400);
+  }
+  if (validator.isEmpty(data.vehicleId)) {
+    return response(res, 'Vehicle Id cannot empty!', null, null, 400);
+  }
+  if (validator.isEmpty(data.rentStartDate)) {
+    return response(res, 'Rent Start Date cannot empty!', null, null, 400);
+  }
+  if (validator.isEmpty(data.rentEndDate)) {
+    return response(res, 'Rent End Date cannot empty!', null, null, 400);
+  }
+  if (validator.isEmpty(data.prepayment)) {
+    return response(res, 'Prepayment cannot empty!', null, null, 400);
+  }
+  if (validator.isEmpty(data.isReturned)) {
+    return response(res, 'Is Returned cannot empty!', null, null, 400);
+  }
+  if (validator.isInt(data.userId)) {
+    if (validator.isInt(data.vehicleId)) {
+      if (validator.isDate(data.rentStartDate)) {
+        if (validator.isDate(data.rentEndDate)) {
+          if (validator.isInt(data.prepayment)) {
+            if (validator.isInt(data.isReturned)) {
+              historyModel.postHistory(data, (results => {
+                if (results.affectedRows == 1) {
+                  historyModel.getHistory(results.insertId, (temp) => {
+                    return response(res, 'Input data history success!', temp[0], null);
+                  });
+                } else {
+                  return response(res, 'Input data history failed!', null, null, 500);
+                }
+              }));
+            } else {
+              return response(res, 'Invalid input, Is Returned must be number!', null, null, 400);
+            }
+          } else {
+            return response(res, 'Invalid input, Prepayment must be number!', null, null, 400);
+          }
+        } else {
+          return response(res, 'Invalid input, Rent End Date format', null, null, 400);
+        }
+      } else {
+        return response(res, 'Invalid input, Rent Start Date format', null, null, 400);
+      }
+    } else {
+      return response(res, 'Invalid input, Vehicle Id must be number!', null, null, 400);
+    }
+  } else {
+    return response(res, 'Invalid input, User Id must be number!', null, null, 400);
+  }
+};
 
 exports.getHistories = (req, res) => {
   let { userName, vehicleName, page, limit } = req.query;
@@ -13,91 +78,44 @@ exports.getHistories = (req, res) => {
       const { total } = count[0];
       const last = Math.ceil(total / limit);
       if (results.length > 0) {
-        return res.json({
-          success: true,
-          message: 'List Histories',
-          results: results,
-          pageInfo: {
-            prev: page > 1 ? `http://localhost:3000/histories?page=${page - 1}` : null,
-            next: page < last ? `http://localhost:3000/histories?page=${page + 1}` : null,
-            totalData: total,
-            currentPage: page,
-            lastPage: last
-          }
+        return response(res, 'List Histories', results, {
+          prev: page > 1 ? `http://localhost:50000/histories?page=${page - 1}` : null,
+          next: page < last ? `http://localhost:5000/histories?page=${page + 1}` : null,
+          totalData: total,
+          currentPage: page,
+          lastPage: last
         });
       } else {
-        return res.status(404).json({
-          success: false,
-          message: 'History list not found',
-          pageInfo: {
-            prev: page > 1 ? `http://localhost:3000/histories?page=${page - 1}` : null,
-            next: page < last ? `http://localhost:3000/histories?page=${page + 1}` : null,
-            totalData: total,
-            currentPage: page,
-            lastPage: last
-          }
-        });
+        return response(res, 'History list not found', null, null, 404);
       }
     });
   });
 };
 
 exports.getHistory = (req, res) => {
-  const id = parseInt(req.params.id);
-  if (!id) {
-    return res.status(400).send({
-      success: false,
-      message: 'Invalid input, Id must be number!'
-    });
-  }
-  if (id > 0) {
-    historyModel.getHistory(id, results => {
-      if (results.length > 0) {
-        return res.json({
-          success: true,
-          message: 'Detail History',
-          results: results[0]
+  const id = req.params.id;
+  if (validator.isInt(id)) {
+    if (id > 0) {
+      historyModel.getHistory(id, results => {
+        const processedResult = results.map((obj) => {
+          if (obj.image !== null) {
+            obj.image = `${APP_URL}/${obj.image}`;
+          }
+          return obj;
         });
-      } else {
-        return res.status(404).json({
-          success: false,
-          message: `History with ID: ${id} not found`
-        });
-      }
-    });
-  } else {
-    return res.status(400).send({
-      success: false,
-      message: 'Id should be a number greater than 0'
-    });
-  }
-};
-
-exports.postHistory = (req, res) => {
-  const data = {
-    userId: parseInt(req.body.userId),
-    vehicleId: parseInt(req.body.vehicleId),
-    rentStartDate: req.body.rentStartDate,
-    rentEndDate: req.body.rentEndDate,
-    prepayment: req.body.prepayment,
-    isReturned: req.body.isReturned
-  };
-  historyModel.postHistory(data, (results => {
-    if (results.affectedRows == 1) {
-      historyModel.getHistory(results.insertId, (temp) => {
-        return res.send({
-          success: true,
-          messages: 'Input data history success!',
-          results: temp[0]
-        });
+        console.log(processedResult);
+        if (results.length > 0) {
+          return response(res, 'List History Users', results, null);
+        } else {
+          return response(res, `User history with ID: ${id} not found`, null, null, 404);
+        }
       });
     } else {
-      return res.status(500).send({
-        success: false,
-        message: 'Input data history failed!'
-      });
+      return response(res, 'Id should be a number greater than 0', null, null, 400);
     }
-  }));
+  } else {
+    return response(res, 'Invalid input, Id must be number!', null, null, 400);
+  }
 };
 
 exports.patchHistory = (req, res) => {
@@ -111,10 +129,7 @@ exports.patchHistory = (req, res) => {
   };
   const id = parseInt(req.params.id);
   if (!id) {
-    return res.status(400).send({
-      success: false,
-      message: 'Invalid input, Id must be number!'
-    });
+    return response(res, 'Invalid input, Id must be number!', null, null, 400);
   }
   if (id > 0) {
     historyModel.getHistory(id, (results => {
@@ -122,31 +137,18 @@ exports.patchHistory = (req, res) => {
         historyModel.patchHistory(data, id, (results => {
           if (results.affectedRows == 1) {
             historyModel.getHistory(id, (results => {
-              return res.send({
-                success: true,
-                messages: 'Updated data history success!',
-                results: results[0]
-              });
+              return response(res, 'Updated data history success!', results[0], null);
             }));
           } else {
-            return res.status(500).send({
-              success: false,
-              message: 'Data history updated failed!'
-            });
+            return response(res, 'Data history updated failed!', null, null, 500);
           }
         }));
       } else {
-        return res.status(404).json({
-          success: false,
-          message: `History with ID: ${id} not found`
-        });
+        return response(res, `History with ID: ${id} not found`, null, null, 404);
       }
     }));
   } else {
-    return res.status(400).send({
-      success: false,
-      message: 'Id should be a number greater than 0'
-    });
+    return response(res, 'Id should be a number greater than 0', null, null, 404);
   }
 };
 
@@ -154,39 +156,24 @@ exports.patchHistory = (req, res) => {
 exports.deleteHistory = (req, res) => {
   const id = parseInt(req.params.id);
   if (!id) {
-    return res.status(400).send({
-      success: false,
-      message: 'Invalid input, Id must be number!'
-    });
+    return response(res, 'Invalid input, Id must be number!', null, null, 400);
   }
   if (id > 0) {
     historyModel.getHistory(id, (results => {
       if (results.length > 0) {
         historyModel.deleteHistory(id, (results => {
           if (results.affectedRows == 1) {
-            return res.send({
-              success: true,
-              message: 'Data user deleted success!'
-            });
+            return response(res, 'Data user deleted success!', null, null);
           } else {
-            return res.status(500).send({
-              success: false,
-              message: 'Data user delete failed!'
-            });
+            return response(res, 'Data user delete failed!', null, null, 500);
           }
         }));
       } else {
-        return res.status(404).json({
-          success: false,
-          message: `History with ID: ${id} not found`
-        });
+        return response(res, `History with ID: ${id} not found`, null, null, 404);
       }
     }));
   } else {
-    return res.status(400).send({
-      success: false,
-      message: 'Id should be a number greater than 0'
-    });
+    return response(res, 'Id should be a number greater than 0', null, null, 400);
   }
 };
 
@@ -196,22 +183,13 @@ exports.popularBasedOnMonth = (req, res) => {
   year = parseInt(year) || null;
   const data = { month, year };
   if (!month && !year) {
-    return res.status(400).send({
-      success: false,
-      message: 'Month and Year must be filled with number'
-    });
+    return response(res, 'Month and Year must be filled with number', null, null, 400);
   }
   if (!month) {
-    return res.status(400).send({
-      success: false,
-      message: 'Month must be filled with number'
-    });
+    return response(res, 'Month must be filled with number', null, null, 404);
   }
   if (!year) {
-    return res.status(400).send({
-      success: false,
-      message: 'Year must be filled with number'
-    });
+    return response(res, 'Year must be filled with number', null, null, 400);
   }
   historyModel.popularBasedonMonth(data, (result) => {
     const cekMonth = (month) => {
@@ -231,22 +209,12 @@ exports.popularBasedOnMonth = (req, res) => {
     };
     cekMonth(month, year).then((cek) => {
       if (result.length > 0) {
-        return res.send({
-          success: true,
-          message: 'Most Popular Vehicles in month ' + cek + ' ' + year,
-          result
-        });
+        return response(res, 'Most Popular Vehicles in month ' + cek + ' ' + year, result, null, null);
       } else {
-        return res.status(404).send({
-          success: false,
-          message: 'Data vehicles not found'
-        });
+        return response(res, 'Data vehicles not found', null, null, 404);
       }
     }).catch((err) => {
-      return res.status(400).send({
-        success: false,
-        message: '' + err + ''
-      });
+      return response(res, '' + err + '', null, null, 400);
     });
   });
 };
