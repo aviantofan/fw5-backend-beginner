176bedf2809e54232926c0be85ea2621d33ff166
eexxprd <aviantofan@gmail.com>
2022-04-20 14:29:32 +0700

    feat: update index routes

--- source/routes/index.js
+++ source/routes/index.js
@@ -1,5 +1,6 @@
 const route = require('express').Router();
 const cors = require('cors');
+const response = require('../helpers/response');
 
 route.use('/vehicles', cors(), require('./vehicles'));
 route.use('/users', cors(), require('./users'));
@@ -7,5 +8,9 @@ route.use('/histories', cors(), require('./histories'));
 route.use('/categories', cors(), require('./categories'));
 route.use('/auth', cors(), require('./auth'));
 route.use('/profile', cors(), require('./profile'));
+
+route.get('/', (req, res) => {
+  return response(res, 'Backend Is Running Well', null, null);
+});
 
 module.exports = route;
