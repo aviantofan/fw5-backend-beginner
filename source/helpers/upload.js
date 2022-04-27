a431c7d0ce3d181d06ab9a762cf867c231204022
eexxprd <aviantofan@gmail.com>
2022-04-27 07:27:37 +0700

    feat: add upload with cloudinary

--- source/helpers/upload.js
+++ source/helpers/upload.js
@@ -1,15 +1,46 @@
 const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
+const { nanoid } = require('nanoid');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    const _file = file.originalname.split('.');
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, _file[0] + '-' + uniqueSuffix + '.' + _file[1]);
+const {
+  CLOUD_NAME,
+  CLOUD_API_KEY,
+  CLOUD_API_SECRET,
+} = process.env;
+
+let storage;
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_API_SECRET,
+  secure: true
+});
+
+storage = new CloudinaryStorage({
+  cloudinary,
+  params:{
+    folder : 'vehicool/uploads',
+    format: async (req, file) => 'jpg',
+    public_id: async (req, file) => {
+      const currentDate = Date.now();
+      const fileName = String(nanoid(10)) + '-' + currentDate;
+      return fileName;
+    }
   }
 });
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads/');
+//   },
+//   filename: function (req, file, cb) {
+//     const _file = file.originalname.split('.');
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//     cb(null, _file[0] + '-' + uniqueSuffix + '.' + _file[1]);
+//   }
+// });
 
 
 const fileFilter = (req, file, cb) => {
